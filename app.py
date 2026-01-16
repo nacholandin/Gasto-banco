33e6a0794ce70921841d40bb68f1ebb378a8228a
Nacho <ignaciolandin@hotmail.com>
2026-01-16 20:57:47 +0100

    Primer commit

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import plotly.express as px
+from module.aplication import *
+from eda import eda_app
+from about import about_app
+from PIL import Image
+ 
+def main():
+    st.set_page_config(**PAGE_CONFIG)
+   
+    menu = ["Home", "Gastos BBVA"]
+
+    choice = st.sidebar.selectbox(label = "Menu", options = menu, index = 0)
+ 
+    if choice == "Home":
+        
+        
+        st.header(body = "Introducción")
+
+        st.subheader("DESGLOSE GASTOS CUENTA BANCO BBVA")
+
+        image = Image.open("source/Colorful Modern Line Chart Diagram Graph.png")
+        st.image(image           = image, 
+                 width=500)
+        
+
+    elif choice == "Exploratory Data Analysis":
+        eda_app()
+
+
+
+if __name__ == "__main__":
+    main()
+
